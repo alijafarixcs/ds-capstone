0d3f687f85cf22af4c2d14b333744aeea477a82b
alijafarixcs <alijafarixcs@gmail.com>
2024-07-02 11:28:33 +0200

    'doc2vec'

--- notebooks/lib/mydoc2vec.py
+++ notebooks/lib/mydoc2vec.py
@@ -7,15 +7,20 @@ import nltk
 nltk.download('stopwords')
 stop_words = set(stopwords.words('english'))
 
-class MyDoc2VecClass:
-    def __init__(self, vector_size=100, window=5, min_count=2, workers=4, epochs=20):
+class Doc2VecRecommender:
+    def __init__(self,data=None, vector_size=100, window=5, min_count=2, workers=4, epochs=20):
         self.vector_size = vector_size
         self.window = window
         self.min_count = min_count
         self.workers = workers
         self.epochs = epochs
         self.model = None
-
+        self.data=data
+    def recommend_by_text(self,search=[]):
+        lists=[]
+        for i in self.get_similar_indexs([search]):
+            lists.append(self.data.iloc[i[0]])
+        return pd.DataFrame(lists)
     def preprocess(self, text):
         return [word for word in text.split() if word.lower() not in stop_words]
 
@@ -25,7 +30,6 @@ class MyDoc2VecClass:
         self.model = Doc2Vec(vector_size=self.vector_size, window=self.window, min_count=self.min_count, workers=self.workers, epochs=self.epochs)
         self.model.build_vocab(train_docs)
         self.model.train(train_docs, total_examples=self.model.corpus_count, epochs=self.model.epochs)
-        self.model.save("doc2vec_model")
     def load_model(self,path='doc2vec_model'):
          self.model = Doc2Vec.load(path)
     def get_similar_indexs(self,text=["This is a new document to find similar documents for"]):
