16d3ba254f8479b56b6a956a0ad5fa3df78aca24
alijafarixcs <alijafarixcs@gmail.com>
2024-07-02 12:09:38 +0200

    new

--- notebooks/lib/data_prepration.py
+++ notebooks/lib/data_prepration.py
@@ -23,8 +23,9 @@ class DataPreparation:
             self.data = pd.read_csv(self.file_path)
         else:
             self.data =self.read_large_csv(self.file_path,1000)
-        
-        self.data = self.data.drop(columns=['Unnamed: 0.1', 'Unnamed: 0'])
+        remove_cols=['Unnamed: 0.1', 'Unnamed: 0']
+        if all(col in self.data.columns for col in remove_cols):
+            self.data = self.data.drop(columns=['Unnamed: 0.1', 'Unnamed: 0'])
         
         
         self.data['Price'] = self.data['Price'].fillna(np.mean(self.data['Price']))
